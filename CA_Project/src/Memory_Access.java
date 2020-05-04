32443ec9af01ff71ba91ac79155746ec7de3478d
Ali Ali El-Dabaa <eldab3.2015@gmail.com>
2020-05-04 18:11:49 +0200

    MemoryAccess

--- CA_Project/src/Memory_Access.java
+++ CA_Project/src/Memory_Access.java
@@ -1,31 +1,39 @@
 
 public class Memory_Access {
 	DataMemory ram;
+
 	public Memory_Access(DataMemory ram) {
 		this.ram = ram;
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-/*
- * This method takes in the Address which is the ALU result (offset is added
-	to the value in the rs in the ALU). Plus the readdata2 value (this will be our write data in
-	the memory). from InstDecode method, the result data from Execute method as an input.
-	writes the result in the memory address and sets the corresponding flag for memory accessing
- */
-	public int MemoAccess(short ALUresult, short ReadData2, char MemWrite,char MemRead, char Branch, char Zero_Flag, int SignExtend) {
-		if(MemWrite == '1') {
+	/*
+	 * This method takes in the Address which is the ALU result (offset is added to
+	 * the value in the rs in the ALU). Plus the readdata2 value (this will be our
+	 * write data in the memory). from InstDecode method, the result data from
+	 * Execute method as an input. writes the result in the memory address and sets
+	 * the corresponding flag for memory accessing
+	 */
+	public int MemoAccess(short ALUresult, short ReadData2, char MemWrite, char MemRead, char Brancheq, char Branchless,
+			char Zero_Flag, char lessThan_Flag, int SignExtend, short branchAddress) {
+		if (MemWrite == '1') {
 			ram.setMemWrite(ALUresult, ReadData2);
 		}
-		int increment = 4;
-		if(Branch == '1' && Zero_Flag == '1') {
-			increment += SignExtend<<2;
+		
+		if (Brancheq == '1' && Zero_Flag == '1') {
+			Main.PC = branchAddress;
 		}
-		Instruction_Fetch.ProgCount(increment);
-		if(MemRead == '1') {
+		if (Branchless == '1' && lessThan_Flag == '1') {
+			Main.PC = branchAddress;
+		}
+		
+		if (MemRead == '1') {
 			return ram.setMemRead(ALUresult);
 		}
 		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
 }
